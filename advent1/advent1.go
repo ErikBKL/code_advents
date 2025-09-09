d3e4b7b4def343b71d3d0bff59ef16ba6e8101fb
erik.berman <erikbermankleiner@gmail.com>
2025-09-09 15:03:03 +0300

    deleted the function

--- advent1/advent1.go
+++ advent1/advent1.go
@@ -13,38 +13,7 @@ import (
 
 
 func PrepareData(pathToFile string) (map[int]int, map[int]int, error) {
-	file, err := os.Open(pathToFile)
-	defer file.Close()
-	 
-	if err != nil {
-		return nil, nil, err
-	}
 	
-	// foreach line in file
-	scanner := bufio.NewScanner(file)
-	numbers1 := make(map[int]int)
-	numbers2 := make(map[int]int)
-
-	for scanner.Scan() {
-		// split by space/tab delimiter and store to num1 num2
-		line := scanner.Text()
-
-		lineSlice := strings.Fields(line)
-		// append num1 to slice1
-		num1, err := strconv.Atoi(lineSlice[0])
-		if err != nil {
-			return nil, nil, err
-		}
-		num2, err := strconv.Atoi(lineSlice[1])
-		if err != nil {
-			return nil, nil, err
-		}
-
-		numbers1[num1]++
-		numbers2[num2]++
-	}
-
-	return numbers1, numbers2, nil
 }
 
 func TotalDistance(numbers1, numbers2 []int) int {
