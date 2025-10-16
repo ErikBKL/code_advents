1c8e5c1898eb4acf93eafd60ed741ae6b771feea
erik.berman <erikbermankleiner@gmail.com>
2025-10-16 16:55:48 +0300

    not working

--- advent13/advent13.go
+++ advent13/advent13.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"strings"
 	"strconv"
+	"reflect"
 )
 type Point struct{ 
 	X	int 
@@ -12,14 +13,27 @@ type Point struct{
 }
 
 type Challange struct {
-	A     Point
-	B     Point
-	Prize Point
+	A     	Point
+	B     	Point
+	Prize 	Point
+	Moves	[2]int
 }
 
-const APRICE = 3
-const BPRICE = 1
 
+const (
+	APRICE = 3
+	BPRICE = 1
+	OFFSET = 10000000000000
+) 
+ 
+
+func Min( a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
 func LowestPriceForMaxPrizes(pathToFile string) (int,error) {
 	file, err := os.Open(pathToFile)
 	if err != nil {
@@ -31,17 +45,45 @@ func LowestPriceForMaxPrizes(pathToFile string) (int,error) {
 		return 0, err
 	}
 
-	for i, ch := range challanges {
-		if ! IsPointReachable(ch) {
-			continue
-		}
+	ret := 0
+	for _, ch := range challanges {
+		m := MarkAllPoints(ch)
 
-
-
+		prices, ok := m[ch.Prize]
+		if ok {
+			ret += Min(prices[0], prices[1])
+		} 
+			
 	}
 
+	return ret, nil
+}
 
-	return 0, nil
+
+func MarkAllPoints(ch Challange) map[Point][]int {
+	m := map[Point][]int{}
+	maxMoves := 100
+	
+	for a := 1 ; a <= maxMoves ; a++ {
+		for b := 0 ; b <= maxMoves ; b++ {
+			pointToInsert := Point{
+				X: ch.A.X * a + ch.B.X * (b), 
+				Y: ch.A.Y * a + ch.B.Y * (b),
+			}
+	
+			m[pointToInsert] = append(m[pointToInsert], a * APRICE + b * BPRICE)
+
+			pointToInsert = Point{
+				X: ch.B.X * a + ch.A.X * (b), 
+				Y: ch.B.Y * a + ch.A.Y * (b),
+			}
+
+			m[pointToInsert] = append(m[pointToInsert], a * BPRICE + b * APRICE)
+
+		}
+	}
+
+	return m
 }
 
 func ReadFile(file *os.File ) ([]Challange, error) {
@@ -82,8 +124,7 @@ func ReadFile(file *os.File ) ([]Challange, error) {
 		}
 	}
 
-	
-	if ch != emptyChallange {
+	if !reflect.DeepEqual(ch, emptyChallange) {
 		challange = append(challange, ch)
 	}
 
@@ -112,8 +153,112 @@ func ExtractNumbers(line, separator string) (int, int, error) {
 
 }
 
-func IsPointReachable(ch Challange) bool {
 
-	if ch.Prize.X % ch.A.
-	return true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+func ReadFilePt2(file *os.File ) ([]Challange, error) {
+	scanner := bufio.NewScanner(file)
+	challange := []Challange{}
+
+	// foreach line in file:
+	ch := Challange{}
+	emptyChallange := Challange{}
+	for scanner.Scan() {
+		// separate by : - discard left - keep right
+		line := scanner.Text()
+		switch {
+		case strings.Contains(line, "Button A:"):
+			x,y,err := ExtractNumbers(line, "+")
+			if err != nil {
+				return nil, err
+			}
+			ch.A.X = x
+			ch.A.Y = y
+		case strings.Contains(line, "Button B:"):
+			x,y,err := ExtractNumbers(line, "+")
+			if err != nil {
+				return nil, err
+			}
+			ch.B.X = x
+			ch.B.Y = y
+		case strings.Contains(line, "Prize:"):
+			x,y,err := ExtractNumbers(line, "=")
+			if err != nil {
+				return nil, err
+			}
+			ch.Prize.X = x + OFFSET
+			ch.Prize.Y = y + OFFSET
+		default:
+			challange = append(challange, ch)
+			ch = emptyChallange
+		}
+	}
+
+	
+	if !reflect.DeepEqual(ch, emptyChallange) {
+		challange = append(challange, ch)
+	}
+
+	return challange, nil
+}
+
+
+
+func LowestPriceForMaxPrizesPt2(pathToFile string) (int,error) {
+	file, err := os.Open(pathToFile)
+	if err != nil {
+		return 0, err
+	}
+
+	challanges, err := ReadFilePt2(file)
+	if err != nil {
+		return 0, err
+	}
+
+	ret := 0
+	for _, ch := range challanges {
+		prices := []int{}
+		prices = MinPriceForChallange(ch, ch.Prize, Point{0,0}, prices)
+		if len(prices) > 1 {
+			ret += Min(prices[0], prices[1])
+		}
+	}
+
+	return ret, nil
+}
+
+func MinPriceForChallange(ch Challange, target, current Point, prices []int) []int {
+	buttons := []Point{ch.A, ch.B}
+
+	for i,button := range buttons {
+
+		if current.Y > target.Y || current.X > target.X {
+			return prices
+		} 
+	
+		if current == target {
+			prices = append(prices, APRICE * ch.Moves[0] + BPRICE * ch.Moves[1])
+			return prices
+		}
+		
+		ch.Moves[i]++
+		prices = MinPriceForChallange(ch, ch.Prize, Point{X: current.X + button.X, Y: current.Y + button.Y}, prices)
+		ch.Moves[i]--
+
+	}
+	
+	return prices
 }
--- advent13/advent13_test.go
+++ advent13/advent13_test.go
@@ -6,45 +6,99 @@ import (
 	"os"
 )
 
-func Test(t *testing.T) {
-	got, _ := LowestPriceForMaxPrizes("./test.txt")
-	want := 1
+// func Test(t *testing.T) {
+// 	got, _ := LowestPriceForMaxPrizes("./test.txt")
+// 	want := 1
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
-}
+// 	if got != want {
+// 		t.Errorf("got %d want %d", got, want)
+// 	}
+// }
 
 func TestReadFile(t *testing.T) {
 	file, err := os.Open("./test.txt")
 	if err != nil {
 		t.Fatalf("Unexpected error")
 	}
+	defer file.Close()
 
-	got, err := ReadFile(file)
+
+	challanges, err := ReadFile(file)
 	if err != nil {
 		t.Fatalf("Unexpected error")
 	}
 
 	want := []Challange{
+		// {
+		// 	A: 		Point{X: 2, Y: 4},
+		// 	B: 		Point{X: 4, Y: 2},
+		// 	Prize: 	Point{X: 4, Y: 8},
+		// },
 		{
 			A: 		Point{X: 94, Y: 34},
 			B: 		Point{X: 22, Y: 67},
 			Prize: 	Point{X: 8400, Y: 5400},
 		},
 		{
-			A: 		Point{X: 26, Y: 57},
-			B: 		Point{X: 51, Y: 13},
-			Prize: 	Point{X: 15496, Y: 17815},
+			A: 		Point{X: 26, Y: 66},
+			B: 		Point{X: 67, Y: 21},
+			Prize: 	Point{X: 12748, Y: 12176},
 		},
 		{
-			A: 		Point{X: 20, Y: 81},
-			B: 		Point{X: 66, Y: 14},
-			Prize: 	Point{X: 13062, Y: 10478},
+			A: 		Point{X: 17, Y: 86},
+			B: 		Point{X: 84, Y: 37},
+			Prize: 	Point{X: 7870, Y: 6450},
+		},
+		{
+			A: 		Point{X: 69, Y: 23},
+			B: 		Point{X: 27, Y: 71},
+			Prize: 	Point{X: 18641, Y: 10279},
 		},
 	}
 
-	if !slices.Equal(got, want) {
-		t.Errorf("got %+v want %+v", got, want)
+	t.Run("ReadFile", func (t *testing.T){
+		if !slices.Equal(challanges, want) {
+			t.Errorf("got %+v want %+v", challanges, want)
+		}
+	})
+	
+	t.Run("MarkAllPoints", func (t *testing.T){
+		
+		got := 0
+		want := 0
+		m := map[Point][]int{}
+
+		for _,ch := range challanges {
+			m = MarkAllPoints(ch)
+			got = len(m)
+			want = 35
+		}
+
+		if got != want {
+			// t.Errorf("%+v", m)
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+}
+
+func TestLowestPriceForMaxPrizes(t *testing.T) {
+	got, _ := LowestPriceForMaxPrizes("./test.txt")
+	want := 480
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+
+func TestLowestPriceForMaxPrizesPt2(t *testing.T) {
+	got, err := LowestPriceForMaxPrizesPt2("./test.txt")
+	if err != nil {
+		t.Fatalf("Unexpected error")
+	}
+	want := 480
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
 	}
 }
