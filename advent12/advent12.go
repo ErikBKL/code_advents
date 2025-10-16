674bb6f10fd725870fc59cce62dc6581b6d678ef
erik.berman <erikbermankleiner@gmail.com>
2025-10-16 08:33:55 +0300

    YESSSSS SUCCESSSSSSS

--- advent12/advent12.go
+++ advent12/advent12.go
@@ -6,8 +6,6 @@ import (
 	matrix "erikberman.matrix.com"
 )
 
-const VISITED = '.'
-
 // func TotalFencePrice(pathToFile string) (int, error) {
 // 	mtx, err := matrix.FileToMatrix(pathToFile)
 // 	if err != nil {
@@ -85,6 +83,14 @@ func TotalFencePricePt2(pathToFile string) (int, error) {
 	return ret, nil
 }
 
+// check if is corner increment sides
+// if I've already been here return
+// Mark that I've been here and increment area
+// from current pos for all 4 directions:
+	// if next is out of board don't check this direction
+	// Now that I know next is valid, get the next point and the next value
+	// if the next value is different than me - it's not a part of my region, so don't check this direction
+	// Now that I know next is valid and it is just like me - re-enter the function.
 func RecGetRegionPt2(mtx *matrix.Matrix[rune], current matrix.Point, area, sides *int){
 
 	dirs := []matrix.Direction{matrix.UP, matrix.RIGHT, matrix.DOWN, matrix.LEFT}
@@ -93,28 +99,28 @@ func RecGetRegionPt2(mtx *matrix.Matrix[rune], current matrix.Point, area, sides
 	
 	if unicode.IsUpper(currValue) {
 		
-		if IsTopLeftCorner(mtx, current) /* || IsInnerTopLeftCorner(mtx, current)  */{
+		if IsExternCorner(mtx, current, matrix.UP, matrix.LEFT ) {
 			(*sides)++
 		} 
-		if IsTopRightCorner(mtx, current) /* ||  IsInnerTopRightCorner(mtx, current) */ {
+		if IsExternCorner(mtx, current, matrix.DOWN, matrix.LEFT) {
 			(*sides)++
 		} 
-		if IsBottomLeftCorner(mtx, current)/*  || IsInnerBottomLeftCorner(mtx, current) */{
+		if IsExternCorner(mtx, current, matrix.UP, matrix.RIGHT) {
 			(*sides)++
 		} 
-		if IsBottomRightCorner(mtx, current) /* || IsInnerBottomRightCorner(mtx, current) */ {
+		if IsExternCorner(mtx, current, matrix.DOWN, matrix.RIGHT) {
 			(*sides)++
 		} 
-		if IsInnerBottomLeftCorner(mtx, current){
+		if IsInternCorner(mtx, current, matrix.UP, matrix.LEFT, matrix.LEFT_UP){
 			(*sides)++
 		} 
-		if IsInnerBottomRightCorner(mtx, current) {
+		if IsInternCorner(mtx, current, matrix.DOWN, matrix.LEFT, matrix.LEFT_DOWN) {
 			(*sides)++
 		} 
-		if IsInnerTopLeftCorner(mtx, current) {
+		if IsInternCorner(mtx, current, matrix.UP, matrix.RIGHT, matrix.RIGHT_UP) {
 			(*sides)++
 		} 
-		if IsInnerTopRightCorner(mtx, current) {
+		if IsInternCorner(mtx, current, matrix.DOWN, matrix.RIGHT, matrix.RIGHT_DOWN) {
 			(*sides)++
 		}
 	} else {
@@ -131,6 +137,11 @@ func RecGetRegionPt2(mtx *matrix.Matrix[rune], current matrix.Point, area, sides
 		}
 
 		next := mtx.NextPoint(dir, current)
+		nextValue := mtx.At(next.Y, next.X)
+
+		if nextValue != currValue {
+			continue
+		} 
 
 		RecGetRegionPt2(mtx, next, area, sides)
 	}
@@ -143,197 +154,44 @@ func RecGetRegionPt2(mtx *matrix.Matrix[rune], current matrix.Point, area, sides
 
 //outern top left corner 
 // if !up && !left
-func IsTopLeftCorner(mtx *matrix.Matrix[rune], current matrix.Point) bool {
+func IsExternCorner(mtx *matrix.Matrix[rune], current matrix.Point, dir1, dir2 matrix.Direction) bool {
 	currValue := mtx.At(current.Y, current.X)
-	nextUpValue := ' '
-	nextLeftValue := ' '
+	nextValueDir1 := ' '
+	nextValueDir2 := ' '
 
-	if mtx.IsNextValid(matrix.UP, current) {
-		nextUpValue = mtx.At(mtx.NextPoint(matrix.UP, current).Y, mtx.NextPoint(matrix.UP, current).X)
+	if mtx.IsNextValid(dir1, current) {
+		nextValueDir1 = mtx.At(mtx.NextPoint(dir1, current).Y, mtx.NextPoint(dir1, current).X)
 	}
-	if mtx.IsNextValid(matrix.LEFT, current) {
-		nextLeftValue = mtx.At(mtx.NextPoint(matrix.LEFT, current).Y, mtx.NextPoint(matrix.LEFT, current).X)
+	if mtx.IsNextValid(dir2, current) {
+		nextValueDir2 = mtx.At(mtx.NextPoint(dir2, current).Y, mtx.NextPoint(dir2, current).X)
 	}
 
-	if 	(!mtx.IsNextValid(matrix.UP, current) || nextUpValue !=  currValue && nextUpValue != unicode.ToLower(currValue) ) &&
-		(!mtx.IsNextValid(matrix.LEFT, current) || nextLeftValue !=  currValue && nextLeftValue != unicode.ToLower(currValue)) {
+	if 	(!mtx.IsNextValid(dir1, current) || nextValueDir1 !=  currValue && nextValueDir1 != unicode.ToLower(currValue) ) &&
+		(!mtx.IsNextValid(dir2, current) || nextValueDir2 !=  currValue && nextValueDir2 != unicode.ToLower(currValue)) {
 			return true
 		}
 
 		return false
 }
 
-
-
-// //outern top right corner
-// if !up && !right 
-func IsTopRightCorner(mtx *matrix.Matrix[rune], current matrix.Point) bool {
+func IsInternCorner(mtx *matrix.Matrix[rune], current matrix.Point, dir1, dir2, diagonal matrix.Direction) bool {
 	currValue := mtx.At(current.Y, current.X)
-	nextUpValue := ' '
-	nextRightValue := ' '
+	nextValueDir1 := ' '
+	nextValueDir2 := ' '
+	nextValueDiagonal := ' '
 
-	if mtx.IsNextValid(matrix.UP, current) {
-		nextUpValue = mtx.At(mtx.NextPoint(matrix.UP, current).Y, mtx.NextPoint(matrix.UP, current).X)
+	if mtx.IsNextValid(dir1, current) {
+		nextValueDir1 = mtx.At(mtx.NextPoint(dir1, current).Y, mtx.NextPoint(dir1, current).X)
 	}
-	if mtx.IsNextValid(matrix.RIGHT, current) {
-		nextRightValue = mtx.At(mtx.NextPoint(matrix.RIGHT, current).Y, mtx.NextPoint(matrix.RIGHT, current).X)
+	if mtx.IsNextValid(dir2, current) {
+		nextValueDir2 = mtx.At(mtx.NextPoint(dir2, current).Y, mtx.NextPoint(dir2, current).X)
 	}
-
-	if 	(!mtx.IsNextValid(matrix.UP, current) || nextUpValue !=  currValue && nextUpValue != unicode.ToLower(currValue) ) &&
-		(!mtx.IsNextValid(matrix.RIGHT, current) || nextRightValue !=  currValue && nextRightValue != unicode.ToLower(currValue)) {
-			return true
-		}
-
-		return false
-}
-
-
-
-// //outern bottom right corner
-// if !down && !right
-func IsBottomRightCorner(mtx *matrix.Matrix[rune], current matrix.Point) bool {
-	currValue := mtx.At(current.Y, current.X)
-	nextDownValue := ' '
-	nextRightValue := ' '
-
-	if mtx.IsNextValid(matrix.DOWN, current) {
-		nextDownValue = mtx.At(mtx.NextPoint(matrix.DOWN, current).Y, mtx.NextPoint(matrix.DOWN, current).X)
+	if mtx.IsNextValid(diagonal, current) {
+		nextValueDiagonal = mtx.At(mtx.NextPoint(diagonal, current).Y, mtx.NextPoint(diagonal, current).X)
 	}
-	if mtx.IsNextValid(matrix.RIGHT, current) {
-		nextRightValue = mtx.At(mtx.NextPoint(matrix.RIGHT, current).Y, mtx.NextPoint(matrix.RIGHT, current).X)
-	}
-
-	if 	(!mtx.IsNextValid(matrix.DOWN, current) || nextDownValue !=  currValue && nextDownValue != unicode.ToLower(currValue) ) &&
-		(!mtx.IsNextValid(matrix.RIGHT, current) || nextRightValue !=  currValue && nextRightValue != unicode.ToLower(currValue)) {
-			return true
-		}
-
-		return false
-}
-
-// // outern bottom left corner
-// if !down && !left
-func IsBottomLeftCorner(mtx *matrix.Matrix[rune], current matrix.Point) bool {
-	currValue := mtx.At(current.Y, current.X)
-	nextDownValue := ' '
-	nextLeftValue := ' '
-
-	if mtx.IsNextValid(matrix.DOWN, current) {
-		nextDownValue = mtx.At(mtx.NextPoint(matrix.DOWN, current).Y, mtx.NextPoint(matrix.DOWN, current).X)
-	}
-	if mtx.IsNextValid(matrix.LEFT, current) {
-		nextLeftValue = mtx.At(mtx.NextPoint(matrix.LEFT, current).Y, mtx.NextPoint(matrix.LEFT, current).X)
-	}
-
-	if 	(!mtx.IsNextValid(matrix.DOWN, current) || nextDownValue !=  currValue && nextDownValue != unicode.ToLower(currValue) ) &&
-		(!mtx.IsNextValid(matrix.LEFT, current) || nextLeftValue !=  currValue && nextLeftValue != unicode.ToLower(currValue)) {
-			return true
-		}
-
-		return false
-}
-
-// // inner bottom right corner
-// if right && down && !bottom-right
-func IsInnerBottomRightCorner(mtx *matrix.Matrix[rune], current matrix.Point) bool {
-	currValue := mtx.At(current.Y, current.X)
-	nextDownValue := ' '
-	nextRightValue := ' '
-	nextBottomRightValue := ' '
-
-	if mtx.IsNextValid(matrix.DOWN, current) {
-		nextDownValue = mtx.At(mtx.NextPoint(matrix.DOWN, current).Y, mtx.NextPoint(matrix.DOWN, current).X)
-	}
-	if mtx.IsNextValid(matrix.RIGHT, current) {
-		nextRightValue = mtx.At(mtx.NextPoint(matrix.RIGHT, current).Y, mtx.NextPoint(matrix.RIGHT, current).X)
-	}
-	if mtx.IsNextValid(matrix.RIGHT_DOWN, current) {
-		nextBottomRightValue = mtx.At(mtx.NextPoint(matrix.RIGHT_DOWN, current).Y, mtx.NextPoint(matrix.RIGHT_DOWN, current).X)
-	}
-	if 	(mtx.IsNextValid(matrix.DOWN, current) && nextDownValue == currValue) &&
-		(mtx.IsNextValid(matrix.RIGHT, current) && nextRightValue ==  currValue) && 
-		(!mtx.IsNextValid(matrix.RIGHT_DOWN, current) || nextBottomRightValue !=  currValue && nextBottomRightValue != unicode.ToLower(currValue)) {
-			return true
-		}
-
-		return false
-}
-// // inner top right corner
-// if right && up && !top-right
-func IsInnerTopRightCorner(mtx *matrix.Matrix[rune], current matrix.Point) bool {
-	currValue := mtx.At(current.Y, current.X)
-	nextUpValue := ' '
-	nextRightValue := ' '
-	nextTopRightValue := ' '
-
-	if mtx.IsNextValid(matrix.UP, current) {
-		nextUpValue = mtx.At(mtx.NextPoint(matrix.UP, current).Y, mtx.NextPoint(matrix.UP, current).X)
-	}
-	if mtx.IsNextValid(matrix.RIGHT, current) {
-		nextRightValue = mtx.At(mtx.NextPoint(matrix.RIGHT, current).Y, mtx.NextPoint(matrix.RIGHT, current).X)
-	}
-	if mtx.IsNextValid(matrix.RIGHT_UP, current) {
-		nextTopRightValue = mtx.At(mtx.NextPoint(matrix.RIGHT_UP, current).Y, mtx.NextPoint(matrix.RIGHT_UP, current).X)
-	}
-	if 	(mtx.IsNextValid(matrix.UP, current) && nextUpValue == currValue) &&
-		(mtx.IsNextValid(matrix.RIGHT, current) && nextRightValue ==  currValue) && 
-		(!mtx.IsNextValid(matrix.RIGHT_UP, current) || nextTopRightValue !=  currValue && nextTopRightValue != unicode.ToLower(currValue)) {
-			return true
-		}
-
-		return false
-}
-
-// // inner top left corner
-// if left && up && !top-left
-func IsInnerTopLeftCorner(mtx *matrix.Matrix[rune], current matrix.Point) bool {
-	currValue := mtx.At(current.Y, current.X)
-	nextUpValue := ' '
-	nextLeftValue := ' '
-	nextTopLeftValue := ' '
-
-	if mtx.IsNextValid(matrix.UP, current) {
-		nextUpValue = mtx.At(mtx.NextPoint(matrix.UP, current).Y, mtx.NextPoint(matrix.UP, current).X)
-	}
-	if mtx.IsNextValid(matrix.LEFT, current) {
-		nextLeftValue = mtx.At(mtx.NextPoint(matrix.LEFT, current).Y, mtx.NextPoint(matrix.LEFT, current).X)
-	}
-	if mtx.IsNextValid(matrix.LEFT_UP, current) {
-		nextTopLeftValue = mtx.At(mtx.NextPoint(matrix.LEFT_UP, current).Y, mtx.NextPoint(matrix.LEFT_UP, current).X)
-	}
-	if 	(mtx.IsNextValid(matrix.UP, current) && nextUpValue == currValue) &&
-		(mtx.IsNextValid(matrix.LEFT, current) && nextLeftValue ==  currValue) && 
-		(!mtx.IsNextValid(matrix.LEFT_UP, current) || nextTopLeftValue !=  currValue && nextTopLeftValue != unicode.ToLower(currValue)) {
-			return true
-		}
-
-		return false
-}
-
-
-
-
-// // inner bottom left corner
-// if left && bottom && !bottom-left
-
-func IsInnerBottomLeftCorner(mtx *matrix.Matrix[rune], current matrix.Point) bool {
-	currValue := mtx.At(current.Y, current.X)
-	nextDownValue := ' '
-	nextLeftValue := ' '
-	nextBottomLeftValue := ' '
-
-	if mtx.IsNextValid(matrix.DOWN, current) {
-		nextDownValue = mtx.At(mtx.NextPoint(matrix.DOWN, current).Y, mtx.NextPoint(matrix.DOWN, current).X)
-	}
-	if mtx.IsNextValid(matrix.LEFT, current) {
-		nextLeftValue = mtx.At(mtx.NextPoint(matrix.LEFT, current).Y, mtx.NextPoint(matrix.LEFT, current).X)
-	}
-	if mtx.IsNextValid(matrix.LEFT_DOWN, current) {
-		nextBottomLeftValue = mtx.At(mtx.NextPoint(matrix.LEFT_DOWN, current).Y, mtx.NextPoint(matrix.LEFT_DOWN, current).X)
-	}
-	if 	(mtx.IsNextValid(matrix.DOWN, current) && nextDownValue == currValue) &&
-		(mtx.IsNextValid(matrix.LEFT, current) && nextLeftValue ==  currValue) && 
-		(!mtx.IsNextValid(matrix.LEFT_DOWN, current) || nextBottomLeftValue !=  currValue && nextBottomLeftValue != unicode.ToLower(currValue)) {
+	if 	(mtx.IsNextValid(dir1, current) && unicode.ToLower(nextValueDir1) == unicode.ToLower(currValue)) &&
+		(mtx.IsNextValid(dir2, current) && unicode.ToLower(nextValueDir2) == unicode.ToLower(currValue)) && 
+		(!mtx.IsNextValid(diagonal, current) || nextValueDiagonal !=  currValue && nextValueDiagonal != unicode.ToLower(currValue)) {
 			return true
 		}
 
